80cb40a972f408c71c3dbe9b25c8878ea6fc5b48
AzureAD\SrideviSivakumar <AzureAD\sridevi.sivakumar@syncfusion.com>
2021-02-12 14:29:23 +0530

    XAMARIN-38746  getting started sample committed

---
+++ DigitalGauge_Sample/DigitalGauge_Sample/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DigitalGauge_Sample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var characterCount = digitalGauge.Value.Length;
+            digitalGauge.WidthRequest = ((characterCount + 1) * digitalGauge.CharacterWidth) + (characterCount * digitalGauge.CharacterSpacing);
+        }
+    }
+}
